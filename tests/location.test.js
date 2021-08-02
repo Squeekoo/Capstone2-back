918d4ba474bd1cac254052ee820298f2023fc448
Squeekoo <edczjr@gmail.com>
2021-08-02 13:11:31 -0700

    cleaned up test files

--- tests/location.test.js
+++ tests/location.test.js
@@ -2,15 +2,15 @@ process.env.NODE_ENV = "test";
 const request = require('supertest');
 const app = require("../app");
 
-describe("GET /locations/:name", () => {
+describe("GET /location/:id", () => {
     it("Get specific location information", async () => {
-        let name = "Salem";
-        const res = await request(app).get(`/locations/${name}`);
+        let id = "10768823";
+        const res = await request(app).get(`/locations/${id}`);
         expect(res.statusCode).toEqual(200);
     });
 
     it("Should return 404 if name not specified", async () => {
-        const res = await request(app).get(`/locations`);
+        const res = await request(app).get(`/location`);
         expect(res.statusCode).toEqual(404);
     });
 });
